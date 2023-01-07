31425a4a8ba453ba40d3a1553e822db532232c2b
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-07 18:20:16 +0100

    poprawka nulll i pustej tablicy

--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -3,10 +3,13 @@ package pl.javastart.task;
 public class PalindromeService {
 
     public boolean isPalindrome(int[] array) {
-
-        for (int i = 0; i < array.length / 2; i++) {
-            if (array[i] != array[array.length - 1 - i]) {
-                return false;
+        if (array == null || array.length == 0) {
+            return false;
+        } else {
+            for (int i = 0; i < array.length / 2; i++) {
+                if (array[i] != array[array.length - 1 - i]) {
+                    return false;
+                }
             }
         }
         return true;
